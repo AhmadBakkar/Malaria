b0fa849c56a3aebdb3effe2d74cc0647bd5dcfe5
AhmadBakkar <90552904+AhmadBakkar@users.noreply.github.com>
2023-06-13 14:25:01 +0300

    Malaria.py

--- Malaria.py
+++ Malaria.py
@@ -2,7 +2,6 @@
 import pandas as pd
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 from streamlit import components
